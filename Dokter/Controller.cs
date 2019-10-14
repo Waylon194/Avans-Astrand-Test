caec8739b6ca22116a704babc1da51b70ed7ca86
Jasper <j.bonefaas@student.avans.nl>
2019-10-14 17:49:47 +0200

    Added: Dokter controller and CW's in the server

---
+++ Dokter/Controller.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using SharedUtillities;
+using System;
+
+namespace Dokter
+{
+    public class Controller
+    {
+        private HomeScreen homeScreen;
+        private AsyncConnection connection;
+        private JObject data;
+
+        public Controller(HomeScreen homeScreen)
+        {
+            this.homeScreen = homeScreen;
+        }
+
+        //Connect async to the server
+        public void Connect()
+        {
+            connection = new AsyncConnection();
+            connection.Connect();
+            connection.NewMessage += OnMessage;
+        }
+
+        //Load a Json object from the received message
+        private void OnMessage(object sender, string message)
+        {
+            data = JObject.Parse(message);
+        }
+
+        //Retrieve all the client data
+        public void DataRequest()
+        {
+            var data = new
+            {
+                type = "dataRequest"
+            };
+            connection.Write(JObject.FromObject(data));
+        }
+    }
+}
